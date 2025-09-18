ecce308345ab516abe63c418dc2218b8c8df571e
EDWINCSN <c.edwin32@hotmail.com>
2025-09-17 21:10:28 -0500

    el boton modifica la imagen

--- ejercicioGit/ejercicio.js
+++ ejercicioGit/ejercicio.js
@@ -1,3 +1,6 @@
 saludar=function(){
+    let cmpImagenSaludo;
+    cmpImagenSaludo=document.getElementById("imgSaludo");
+    cmpImagenSaludo.src="./imagenes/quiko.gif";
     console.log("Ingresa a Saludar");
 };
