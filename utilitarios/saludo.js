c72e1f6858839e979d52b768910994633e3b9d57
EDWINCSN <c.edwin32@hotmail.com>
2025-09-18 21:02:26 -0500

    implementacion de funciones para reuperar int y float en utilitarios, ademas html se agrega caja de txt edad y estatura

--- utilitarios/saludo.js
+++ utilitarios/saludo.js
@@ -4,6 +4,10 @@ saludar=function(){
     let nombre=recuperarTexto("txtNombre");
     //recuperar valor caja texto de txtApellido
     let apellido=recuperarTexto("txtApellido");
+    //recuperar int
+    let edad=recuperarInt("txtEdad");
+    //recuperar float
+    let estatura=recuperarFloat("txtEstatura")
 };
 
 //funcion para recuperar texto
@@ -11,4 +15,18 @@ recuperarTexto=function(idComponente){
     let componente=document.getElementById(idComponente);
     let valorIngresado=componente.value;
     return valorIngresado; 
+};
+
+//funcion para recuperar valor int
+recuperarInt=function(idComponente){
+    let valorCaja=recuperarTexto(idComponente);
+    let valorEntero=parseInt(valorCaja);
+    return valorEntero;
+};
+
+//funcion para recuperar valor float
+recuperarFloat=function(idComponente){
+    let valorCaja=recuperarTexto(idComponente);
+    let valorFloat=parseFloat(valorCaja);
+    return valorFloat;
 };
