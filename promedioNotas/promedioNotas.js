86a76c2a17ee1cfc0ebe65c3cc281bbd4c457b7b
EDWINCSN <c.edwin32@hotmail.com>
2025-09-23 21:03:29 -0500

    EJERCICIO PROMEDIO VERSION 2, modifica promedioNotas.js

--- promedioNotas/promedioNotas.js
+++ promedioNotas/promedioNotas.js
@@ -10,9 +10,17 @@ calcularPromedioNotas=function(){
 
     mostrarTexto("lblPromedio","Resultado: "+promedio);
 
-    if(promedio > 7){
-        mostrarImagen("imgCalificacion","./exito.gif");
-    }else{
+    if(promedio < 5 && promedio > 0){
+        mostrarTexto("lblCalificacion","REPROBADO");
         mostrarImagen("imgCalificacion","./reprobado.gif");
-    }
+    }else if(promedio >=5 && promedio <=8){
+        mostrarTexto("lblCalificacion","BUEN TRABAJO");
+        mostrarImagen("imgCalificacion","./buenTrabajo.gif");
+    } else if(promedio > 8 && promedio <=10){
+        mostrarTexto("lblCalificacion","EXCELENTE");
+        mostrarImagen("imgCalificacion","./aprobado.gif");
+    }else{
+        mostrarTexto("lblCalificacion","DATOS INCORRECTOS");
+        mostrarImagen("imgCalificacion","./error.gif");
+    }    
 };
