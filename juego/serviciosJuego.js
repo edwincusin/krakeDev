a93ef093a6c010094f0861d46b529d39f07e6d6e
EDWINCSN <c.edwin32@hotmail.com>
2025-09-27 14:20:40 -0500

    creacion de directorios iniciales, y archivos, ademas se implementa las funciones obtenerAleatorio, GenerarElemento, determinar ganador y generarRuta

---
+++ juego/serviciosJuego.js
@@ -0,0 +1,80 @@
+// funcion obtener aleatorio de 1 al 3.
+obtenerAleatorio=function(){
+    let aleatorio=Math.random(3);
+    let numeroMultiplicado=aleatorio*3;
+    let numeroEntero=parseInt(numeroMultiplicado);
+    let resultadoAleatorio=numeroEntero+1;
+    console.log(resultadoAleatorio); // para porbar en la consola
+    return resultadoAleatorio;
+};
+
+//funcion generar elemento genera automaticamente las cadenas piedra, pael , tijera
+generarElemento=function(){
+    let numeroAleatorio=obtenerAleatorio();
+    let cadena;
+    if(numeroAleatorio == 1){
+        cadena= "PIEDRA";
+    }
+    if(numeroAleatorio == 2){
+        cadena="PAPEL";
+    }
+    if(numeroAleatorio == 3){
+        cadena="TIJERA";
+    }
+    console.log(cadena); // para porbar en la consola
+    return cadena;    
+};
+
+//funciona determinar jugador
+determinarganador=function(eleccionJugador1,eleccionJugador2){
+    let resultadoGanador;
+    eleccionJugador1="TIJERA";
+    eleccionJugador2="PAPEL"
+    //combinacion1
+    if((eleccionJugador1 == "PIEDRA") && (eleccionJugador2 == "PIEDRA")){
+        resultadoGanador=0;
+    }
+    if((eleccionJugador1 == "PIEDRA") && (eleccionJugador2 == "PAPEL")){
+        resultadoGanador=2;
+    }
+    if((eleccionJugador1 == "PIEDRA") && (eleccionJugador2 == "TIJERA")){
+        resultadoGanador=1;
+    }
+    //combinacion2
+    if((eleccionJugador1 == "PAPEL") && (eleccionJugador2 == "PIEDRA")){
+        resultadoGanador=1;
+    }
+    if((eleccionJugador1 == "PAPEL") && (eleccionJugador2 == "PAPEL")){
+        resultadoGanador=0;
+    }
+    if((eleccionJugador1 == "PAPEL") && (eleccionJugador2 == "TIJERA")){
+        resultadoGanador=2;
+    }
+    //combinacion3
+    if((eleccionJugador1 == "TIJERA") && (eleccionJugador2 == "PIEDRA")){
+        resultadoGanador=2;
+    }
+    if((eleccionJugador1 == "TIJERA") && (eleccionJugador2 == "PAPEL")){
+        resultadoGanador=1;
+    }
+    if((eleccionJugador1 == "TIJERA") && (eleccionJugador2 == "TIJERA")){
+        resultadoGanador=0;
+    }
+    console.log(resultadoGanador); // para porbar en la consola
+    return resultadoGanador;
+};
+
+//generar ruta de la imagen en base al nombre que recibe piedra pael tiejera.
+generarRuta=function(nombre){
+    let ruta="";
+    if(nombre == "PIEDRA"){
+        ruta="./img/piedra.png";
+    }
+    if(nombre == "PAPEL"){
+        ruta="./img/papel.png";
+    }
+    if(nombre == "TIJERA"){
+        ruta="./img/tijera.png";
+    }
+};
+
