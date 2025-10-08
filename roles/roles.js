28a569f35ba3cbd794a07fb6efd063e9e641f471
EDWINCSN <c.edwin32@hotmail.com>
2025-10-07 22:37:24 -0500

    ROL PAGOS PARTE 2 COMPLETO, IMPLEMENTA FUNCION MOSTRAR EMPLEADOS PARA IMPRIMIR EN UNA TABLA || FUNCIONA OK

--- roles/roles.js
+++ roles/roles.js
@@ -1,7 +1,8 @@
-// ARRAY CON VARIOS OBJETOS CON ATRIBUTOS -- GLOBAL
+// ARRAY GLOBAL CON VARIOS OBJETOS CON ATRIBUTOS INICIALIZADOS
 let empleados = [
     { cedula: "1714616123", nombre: "John", apellido: "Cena", sueldo: 500.0 },
-    { cedula: "0914632123", nombre: "Luisa", apellido: "Gonzalez", sueldo: 900.0 }
+    { cedula: "0914632123", nombre: "Luisa", apellido: "Gonzalez", sueldo: 900.0 },
+    { cedula: "1753081056", nombre: "Edwin", apellido: "Cusin", sueldo: 700.0 }
 ];
 
 //FUNCION MOSTRSR OPCION EMPLEADO 
@@ -9,6 +10,7 @@ mostrarOpcionEmpleado = function() {
     mostrarComponente("divEmpleado");
     ocultarComponente("divRol");
     ocultarComponente("divResumen");
+    mostrarEmpleados(); //MOSTRAR TABLA EMPLEADOS
 };
 
 //FUNCION MOSTRSR OPCION ROL 
@@ -23,4 +25,29 @@ mostrarOpcionResumen = function() {
     ocultarComponente("divEmpleado");
     ocultarComponente("divRol");
     mostrarComponente("divResumen");
+};
+
+// FUNCION MOSTRAR EMPLEADOS 
+mostrarEmpleados = function() {
+    let contenidoTabla = "<table>" +
+        "<tr>" +
+        "<th>CEDULA</th>" +
+        "<th>NOMBRE</th>" +
+        "<th>APELLIDO</th>" +
+        "<th>SUELDO</th>" +
+        "</tr>";
+    let elementoEmpleado;
+    for (let i = 0; i < empleados.length; i++) {
+        elementoEmpleado = empleados[i];
+        contenidoTabla += "<tr>" +
+            "<td>" + elementoEmpleado.cedula + "</td>" +
+            "<td>" + elementoEmpleado.nombre + "</td>" +
+            "<td>" + elementoEmpleado.apellido + "</td>" +
+            "<td>" + elementoEmpleado.sueldo + "</td>" +
+            "</tr>";
+    }
+    contenidoTabla += "</table>"
+
+    mostrarTabla("tablaEmpleados", contenidoTabla);
+
 };
